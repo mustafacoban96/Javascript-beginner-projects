d8705ce9cfce119cafd3e4eae97597d14b57ffa8
mustafacoban96 <mustafa.coban@std.hku.edu.tr>
2023-07-22 15:00:32 +0300

    toast-notification-snackBar

---
+++ Toast-Notification-SnackBar/app.js
@@ -0,0 +1,41 @@
+
+let toastBox = document.querySelector('#toastBox');
+let successBtn = document.querySelector('.success');
+let dangerBtn = document.querySelector('.error');
+let warningsBtn = document.querySelector('.warning');
+
+let successMsg = '<i class="fa-solid fa-circle-check"></i>Successfully submitted';
+let errorMsg = '<i class="fa-solid fa-circle-xmark"></i>Please fix the error!';
+let invalidMsg = '<i class="fa-solid fa-circle-exclamation"></i>Invalid input, check again';
+
+
+function showToast(msg){
+
+    let toast = document.createElement('div');
+    toast.classList.add('Mytoast');
+    toast.innerHTML = msg;
+    toastBox.appendChild(toast);
+
+    if(msg.includes('error')){
+        toast.classList.add('error');
+    }
+    if(msg.includes('Invalid')){
+        toast.classList.add('invalid');
+    }
+
+    setTimeout(() => {
+        toast.remove();
+    }, 4000);
+}   
+
+successBtn.addEventListener('click',function(){
+   
+    showToast(successMsg);
+});
+dangerBtn.addEventListener('click',function(){
+    showToast(errorMsg);
+});
+warningsBtn.addEventListener('click',function(){
+    showToast(invalidMsg);
+});
+
