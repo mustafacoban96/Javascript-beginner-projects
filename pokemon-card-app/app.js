205397fd114aa4ddcbe13ab2685d5da931b54a17
mustafacoban96 <mustafa.coban@std.hku.edu.tr>
2023-07-30 13:28:19 +0300

    pokemon-card-app was completed

--- pokemon-card-app/app.js
+++ pokemon-card-app/app.js
@@ -6,17 +6,34 @@ const hp = document.querySelector('.hp');
 const pokeName = document.getElementById('poke-name');
 const type1 = document.getElementById('typ1');
 const type2 = document.getElementById('typ2');
-const attack = document.getElementById('attack');
+const attack = document.getElementById('attack'); 
 const defense = document.getElementById('defense');
 const speed = document.getElementById('speed');
 const generateBtn = document.getElementById('btn');
 const pokeImage = document.getElementById('poke-img');
+const types = document.querySelector('.types');
+const card = document.querySelector('#card')
 
 
-let num = Math.floor(Math.random()*20)
 
 function place(pokemon){
     pokeName.innerHTML = pokemon.name;
+    hp.innerHTML = `<span>HP</span> ${pokemon.stats[0].base_stat}`;
+    attack.innerHTML = pokemon.stats[1].base_stat;
+    defense.innerHTML = pokemon.stats[2].base_stat;
+    speed.innerHTML = pokemon.stats[5].base_stat;
+    if(pokemon.types.length === 1){
+        types.innerHTML = `<span id='typ'>${pokemon.types[0].type.name}</span>`
+        document.getElementById('typ').style.backgroundColor = `var(--${pokemon.types[0].type.name})`;
+    }else{
+        type1.innerHTML = pokemon.types[0].type.name;
+        type2.innerHTML = pokemon.types[1].type.name;
+        type1.style.backgroundColor = `var(--${pokemon.types[0].type.name})`;
+        type2.style.backgroundColor = `var(--${pokemon.types[0].type.name})`;
+    }
+    card.style.background =  `radial-gradient(circle at 50% 0%, var(--${pokemon.types[0].type.name}) 36%,#ffffff 36%)`;
+    pokeImage.src = `${pokemon.sprites.other['official-artwork'].front_shiny}`
+
 }
 
 
@@ -27,10 +44,15 @@ function getExternalApi(number){
     .then(response =>{
         return response.json()
     }).then(data =>{
-        console.log(data)
        place(data);
     })
 }
 
-getExternalApi(num);
 
+
+
+
+generateBtn.addEventListener('click',function(){
+    let num = Math.floor(Math.random()*100)+1
+    getExternalApi(num);
+})
---
+++ pokemon-card-app/readme.md
@@ -0,0 +1,6 @@
+## Pokemon-Card-App
+
+### Screenshots
+
+<img src="https://github.com/mustafacoban96/Javascript-beginner-projects/blob/master/dictionary-app/screenshots/1.png" width="450" height="350" /> 
+
